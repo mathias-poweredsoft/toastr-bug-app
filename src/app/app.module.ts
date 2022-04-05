1b617b7b4285a2076be8f3690946ae76f21c83de
Mathias Beaulieu-Duncan <mathias@poweredsoft.com>
2022-04-05 13:35:15 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+import { CustomToastComponent } from './components/custom-toast/custom-toast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomToastComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      toastComponent: CustomToastComponent
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/custom-toast/custom-toast.component.ts
@@ -0,0 +1,55 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { Toast } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-custom-toast',
+  templateUrl: './custom-toast.component.html',
+  styleUrls: ['./custom-toast.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('inactive', style({
+        opacity: 0,
+      })),
+      transition('inactive => active', animate('400ms ease-out', keyframes([
+        style({
+          transform: 'translate3d(100%, 0, 0) skewX(-30deg)',
+          opacity: 0,
+        }),
+        style({
+          transform: 'skewX(20deg)',
+          opacity: 1,
+        }),
+        style({
+          transform: 'skewX(-5deg)',
+          opacity: 1,
+        }),
+        style({
+          transform: 'none',
+          opacity: 1,
+        }),
+      ]))),
+      transition('active => removed', animate('400ms ease-out', keyframes([
+        style({
+          opacity: 1,
+        }),
+        style({
+          transform: 'translate3d(100%, 0, 0) skewX(30deg)',
+          opacity: 0,
+        }),
+      ]))),
+    ]),
+  ],
+  preserveWhitespaces: false,
+})
+export class CustomToastComponent extends Toast {
+    // used for demo purposes
+    undoString = 'undo';
+  
+    action(event: Event) {
+      event.stopPropagation();
+      this.undoString = 'undid';
+      this.toastPackage.triggerAction();
+      return false;
+    }
+}
